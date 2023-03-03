a2562f6ec611ff723d3da5653f55cbd08a254538
Kabilash Satheesh Kumar <iamkabilash@gmail.com>
2023-03-03 11:51:35 +0530

    refactored code into folders

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ h1: "kabilash" });
-});
+require("./services/passport");
+
+const authRoutes = require("./routes/authRoutes");
+authRoutes(app);
+// require("./routes/authRoutes")(app) // same as the above 2 lines
 
 const PORT = process.env.PORT || 5500;
 app.listen(PORT);
